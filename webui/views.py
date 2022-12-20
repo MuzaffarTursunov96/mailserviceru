12325b5bdbca0d877ba85a0ef5a54931d8013c2d
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2022-12-20 16:14:21 +0300

    login

--- webui/views.py
+++ webui/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
 from main.models import Mails,Messages
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate,login,logout
+
+from django.contrib.auth.decorators import login_required
 
 
+
+
+
+@login_required(login_url='login')
 def index(request):
+    return render(request,'index.html')
+     
+
+def login_view(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    user = authenticate(request, email=email, password=password)
+    if user is not None:
+        login(request, user)
+    else:
+        redirect('login')
     
 
-    return render(request,'index.html')
+def logout_view(request):
+    logout(request)
