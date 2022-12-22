fbbdb219675bc7ad67e4d2b84501ab40a26da246
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-12-22 21:10:45 +0300

    as

--- webui/forms.py
+++ webui/forms.py
@@ -1,15 +1,15 @@
 from django import forms
 from main.models import Mails,Messages
-from MinimalSplitDateTimeMultiWidget import MinimalSplitDateTimeMultiWidget
-from django.forms import DateTimeField
+from django.contrib.admin import widgets
 
 
 
 
 class MailForm(forms.ModelForm):
+  start_time = forms.DateField(widget=widgets.AdminTimeWidget)
+  end_time = forms.DateField(widget=widgets.AdminTimeWidget)
   class Meta:
     model =Mails
     fields =['start_date','text_approval','fil_code_teg','end_date']
 
-  start_date = DateTimeField(widget=MinimalSplitDateTimeMultiWidget())
-  end_date = DateTimeField(widget=MinimalSplitDateTimeMultiWidget())
+  
