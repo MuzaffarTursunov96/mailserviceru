264d092bfcd2ce351afa874fbf7a7de63912f7c3
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-12-23 11:30:42 +0300

    abs

--- webui/forms.py
+++ webui/forms.py
@@ -6,8 +6,12 @@ from django.contrib.admin import widgets
 
 
 class MailForm(forms.ModelForm):
-  start_date = forms.DateField(widget=widgets.AdminTimeWidget)
-  end_date = forms.DateField(widget=widgets.AdminTimeWidget)
+  def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
   class Meta:
     model =Mails
     fields =['start_date','text_approval','fil_code_teg','end_date']
