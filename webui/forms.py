f94750cb716a37df14e7b871940a37f669e410df
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-12-23 12:41:51 +0300

    dsdd

--- webui/forms.py
+++ webui/forms.py
@@ -6,16 +6,17 @@ from accounts.models import Customer
 
 
 class MailForm(forms.ModelForm):
+  start_date =forms.DateField(widget=widgets.AdminTimeWidget)
   def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs.update({
                 'class': 'form-control'
             })
-            if field =='start_date' or field =='end_date':
-               self.fields[field].widget.attrs.update({
-                'class': 'datepicker'
-            })
+            # if field =='start_date' or field =='end_date':
+            #    self.fields[field].widget.attrs.update({
+            #     'class': 'datepicker'
+            # })
   class Meta:
     model =Mails
     fields =['start_date','text_approval','fil_code_teg','end_date']
