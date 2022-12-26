a06909dcf55007d7899f203b9b8df4cb2eab7f46
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-12-26 05:43:44 +0300

    files

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
@@ -44,25 +44,22 @@ class DeleteCustomers(generics.DestroyAPIView):
 @login_required(login_url='login')
 def my_profile(request,pk):
     user =get_object_or_404(User,id=pk)
-    if request.method =="POST":
-        if UserProfile.objects.filter(user=user).exists():
-            profile =UserProfile.objects.get(user=user)
-            form =ProfileForm(request.POST,request.FILES,instance=profile)
-            if form.is_valid():
-                form.save()
-            else:
-                print(form.errors)
-        else:
-            profile =UserProfile(user=user)
-            profile.save()
-            form =ProfileForm(instance=profile)
+    if UserProfile.objects.filter(user=user).exists():
+        profile =UserProfile.objects.get(user=user)
     else:
-        if UserProfile.objects.filter(user=user).exists():
-            profile =UserProfile.objects.get(user=user)
-            form =ProfileForm(instance=profile)
-        else:
-            form =ProfileForm()
+        profile =UserProfile(user=user)
+        profile.save()
 
+    form =ProfileForm(instance =profile)
+
+    if request.method =="POST":
+        formupdate =ProfileForm(request.POST,request.FILES,instance=profile)
+        if formupdate.is_valid():
+            formupdate.save()
+            # messgaes
+            return redirect('my_profile',pk)
+        
+    
     context={
         # 'profile':profile,
         'form':form
